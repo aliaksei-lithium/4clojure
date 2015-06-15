eae41160c806db62b063133dce5e15175a2eb9f3
Aliaksei Zhynhiarouski <blackredprince@gmail.com>
2015-06-16 00:05:11 +0300

    Solve some elementary

--- elem.clj
+++ elem.clj
@@ -1,12 +1,16 @@
+;; 1
 (= true true)
-
+;; 2
 (= (- 10 (* 2 3)) 4)
-
+;; 3
 (= "HELLO WORLD" (.toUpperCase "hello world"))
-
+;; 3
 (= (list ':a ':b ':c) '(:a :b :c))
 
 (def v '(1 2 3))
+
+;; conj add item to vector tail
+;; conj add item to list head
 
 (conj v 8)
 
@@ -17,3 +21,14 @@
 (seq k)
 
 (= [:a :b :c] (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c))
+
+;;8 set
+(= (set '(:a :b :c :d)) (set '(:a :a :b :c :c :c :c :d :d)))
+(= (set '(:a :b :c :d)) (clojure.set/union #{:a :b :c} #{:b :c :d}))
+
+;;9 set
+(= #{1 2 3 4} (conj #{1 4 3} 1 2 ))
+
+;;10 maps
+(= 20 ((hash-map :a 10, :b 20, :c 30) :b))
+(:b {:b 10, :a "a"})
