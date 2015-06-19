c1865f72a6570f7dbce239112cbd31a34a004b20
Aliaksei Zhynhiarouski <blackredprince@gmail.com>
2015-06-20 01:09:52 +0300

    day-to-day update

--- elem.clj
+++ elem.clj
@@ -32,3 +32,50 @@
 ;;10 maps
 (= 20 ((hash-map :a 10, :b 20, :c 30) :b))
 (:b {:b 10, :a "a"})
+
+;;11 conj maps
+(= {:a 1, :b 2, :c 3} (conj {:a 1} [:b 2] [:c 3]))
+
+;;12 seqs
+(= 3 (first '(3 2 1)))
+
+;;13 rest return all except first
+(= [10, 20, 30, 50] (rest [0, 10, 20, 30, 50]))
+
+
+;;14 func
+
+(= 8 ((fn add-five [x] (+ x 5)) 3))
+
+(= 8 ((fn [x] (+ x 5)) 3))
+(= 8 (#(+ % 5) 3))
+(= 8 ((partial + 5) 3))
+
+;; func2
+(= (* 2 11) 22)
+
+;; hello
+(= (#(str "Hello, " % "!") "Lithium") "Hello, Lithium!")
+
+;; seq map
+(= '(6 7 8) (map #(+ % 5) '(1 2 3)))
+
+;; seq filter
+(= '(6 7) (filter #(> % 5) '(3 4 5 6 7)))
+
+
+;; get last
+
+(= ((fn [ar] (get ar (- (count ar) 1))) [1 2 3 4 5]) 5)
+(= (#(nth % (- (count %) 1)) [1 2 3 4 5]) 5)
+(= (#(nth % (- (count %) 1)) '(5 4 3)) 3)
+(= (#(nth % (- (count %) 1)) ["b" "c" "d"]) "d")
+
+;; return the second from last 
+(comp second reverse)
+
+;;  returns the Nth element from a sequence.
+(= (#(first (drop %2 %1)) '(4 5 6 7) 2) 6)
+
+;;returns the total number of elements in a sequence.
+#(reduce (fn [a b] inc b) 0 %1)
